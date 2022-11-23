8ee9cf6c372a00b2c6dd73225fac4ede95ba08a4
dvsni <danil.shir@outlook.com>
2022-11-23 15:50:05 +0300

    task final

--- Program.cs
+++ Program.cs
@@ -6,4 +6,36 @@
 //При решение не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
 
+Console.WriteLine("Введите количество элементов");
+int size = Convert.ToInt32(System.Console.ReadLine());
 
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
+
+string[] arrayElements = new string[size];
+int leng = 3;
+int position = 0;
+for (int k = 0; k < size; k++)
+{
+    if (arrayStrings[k].Length <= leng)
+    {
+        arrayElements[position] = arrayStrings[k];
+        position++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayElements);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Элементы соответствующие требованиям массива [{array[i]}]");
+    }
+    Console.WriteLine();
+}
